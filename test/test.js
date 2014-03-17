a813dc56618e776c806131443119708e67ae0d37
Daniel <dano.husar@gmail.com>
2014-03-17 13:14:18 +0000

    add search actor

--- test/test.js
+++ test/test.js
@@ -4,16 +4,19 @@ var should = require('should'),
   fs = require('fs');
 
 describe('Globals: ', function(){
-  it('CSFD object should ahve all methods', function () {
+  it('CSFD object should have all methods', function () {
     csfd.should.be.type('object');
     csfd.searchMovie.should.be.type('function');
   });
 });
 
+/**
+ * Search movie tests
+ */
 describe('Search Movie fake data', function(){
   it('We should get proper movie', function (done) {
 
-    fs.readFile('test/fixtures/search.html', function (err, data) {
+    fs.readFile('test/fixtures/searchMovie.html', function (err, data) {
       var temp =  csfd.getUrl;
       csfd.getUrl = function(url, cb){
         cb(data);
@@ -51,4 +54,44 @@ describe('Search Movie real data', function(){
   });
 });
 
+/**
+ * Search actor tests
+ */
+describe('Search Actor fake data', function(){
+  it('We should get proper actor', function (done) {
 
+    fs.readFile('test/fixtures/searchActor.html', function (err, data) {
+      var temp =  csfd.getUrl;
+      csfd.getUrl = function(url, cb){
+        cb(data);
+      };
+
+      csfd.searchActor('Brad Pitt').then(function(data){
+        data[0].title.should.be.equal('Brad Pitt');
+        data[0].url.should.be.equal('http://www.csfd.cz/tvurce/5-brad-pitt/');
+        data[0].image.should.be.equal('//img.csfd.cz/files/images/creator/photos/000/267/267330_0640be.jpg?w100h132crop');
+        data[0].id.should.be.equal('5');
+        data[0].roles[0].should.be.equal('herec');
+        data[0].dateBirth.should.be.equal('18.12.1963');
+        data[0].placeBirth.should.be.equal('Shawnee, Oklahoma, USA');
+        csfd.getUrl = temp;
+        done();
+      });
+    });
+  });
+});
+
+describe('Search Actor real data', function(){
+  it('We should get proper actor', function (done) {
+    csfd.searchActor('Brad Pitt').then(function(data){
+      data[0].title.should.be.equal('Brad Pitt');
+      data[0].url.should.be.equal('http://www.csfd.cz/tvurce/5-brad-pitt/');
+      data[0].image.should.be.equal('//img.csfd.cz/files/images/creator/photos/000/267/267330_0640be.jpg?w100h132crop');
+      data[0].id.should.be.equal('5');
+      data[0].roles[0].should.be.equal('herec');
+      data[0].dateBirth.should.be.equal('18.12.1963');
+      data[0].placeBirth.should.be.equal('Shawnee, Oklahoma, USA');
+      done();
+    });
+  });
+});
