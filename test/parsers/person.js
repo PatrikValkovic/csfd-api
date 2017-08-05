ce60141969f22d793deafed2d0f4db684f790cba
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-05 17:15:16 +0200

    Add files for tests of parsers

---
+++ test/parsers/person.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Patrik Valkovic
+ * 8/5/17.
+ */
+
+'use strict'
+
+const personParser = require('../../lib/parsers').person
+
+
+describe('Parsing of sites about person', function () {
+
+})
---
+++ test/parsers/search.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Patrik Valkovic
+ * 8/5/17.
+ */
+
+'use strict'
+
+
+const searchParser = require('../../lib/parsers').search
+
+
+describe('Parsing of search sites', function () {
+
+})
