54f956943dc4a8021c9ccacc752a0ca11cd483a2
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-08 14:00:33 +0200

    Base implementation of search parsing

--- test/parsers/search.js
+++ test/parsers/search.js
@@ -12,14 +12,17 @@ const searchParser = require('../../lib/parsers').search
 
 describe('Parsing of search sites', function () {
 
-  it('Should parse Quentin Tarantino from search page', async function () {
+  it('Should parse Quentin Tarantino from search page', function () {
     const content = fs.readFileSync(`${__dirname}/sites/QuentinTarantinoSearch.html`)
-    const parsed = await searchParser(content)
+    const parsed = searchParser(content)
     assert.deepStrictEqual(parsed, {
       films: [
-        {id: 308772, name: 'Tarantino\'s Mind'},
-        {id: 322063, name: 'Tarantino, le disciple de Hong-Kong'},
-        {id: 63695, name: 'Tarantino XX - 20 Years of Filmmaking'},
+        {'id': 308772, 'name': 'Tarantino\'s Mind'},
+        {'id': 343390, 'name': 'Brooklyn 99 - Paranoia'},
+        {'id': 322063, 'name': 'Tarantino, le disciple de Hong-Kong'},
+        {'id': 63695, 'name': 'Tarantino XX - 20 Years of Filmmaking'},
+        {'id': 102791, 'name': 'Vzjať Tarantinu'},
+        {'id': 166642, 'name': 'Quentin Tarantino\'s Star Wars'},
       ],
       people: [
         {id: 2120, name: 'Quentin Tarantino'},
@@ -30,8 +33,9 @@ describe('Parsing of search sites', function () {
     })
   })
 
-  it('Should parse Pirates of the Caribbean film from search page', async function () {
+  it('Should parse Pirates of the Caribbean film from search page', function () {
     const content = fs.readFileSync(`${__dirname}/sites/PiratesOfTheCaribbeanSearch.html`)
+    const parsed = searchParser(content)
     assert.deepStrictEqual(parsed, {
       films: [
         {id: 290818, name: 'Piráti z Karibiku: Salazarova pomsta'},
@@ -241,9 +245,9 @@ describe('Parsing of search sites', function () {
     })
   })
 
-  it('Should parse Kit Harington from search page', async function () {
+  it('Should parse Kit Harington from search page', function () {
     const content = fs.readFileSync(`${__dirname}/sites/KitHaringtonSearch.html`)
-    const parsed = await searchParser(content)
+    const parsed = searchParser(content)
     assert.deepStrictEqual(parsed, {
       films: [],
       people: [
