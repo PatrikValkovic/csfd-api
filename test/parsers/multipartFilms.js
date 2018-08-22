f374c9805f734db67b37b021b7a1ad4b1217781f
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2018-08-22 17:39:53 +0200

    Fix issue with film parsing for issue #3

---
+++ test/parsers/multipartFilms.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Patrik Valkovic
+ * 21.11.2017.
+ */
+
+'use strict'
+
+const fs = require('fs')
+const chai = require('chai')
+chai.use(require('chai-subset'))
+const filmParser = require('../../lib/parsers').film
+
+describe('Should parse films with multiple parts', function () {
+  it('Arn', function () {
+    const content = fs.readFileSync(`${__dirname}/sites/Arn.html`)
+    const parsed = filmParser(content)
+
+    chai.assert.containSubset(parsed, {
+      length: 2 * 75,
+      serialLength: {
+        episodes: 2,
+        length: 75,
+      },
+    })
+  })
+
+  it('Dinotopie', function () {
+    const content = fs.readFileSync(`${__dirname}/sites/Dinotopie.html`)
+    const parsed = filmParser(content)
+
+    chai.assert.containSubset(parsed, {
+      length: 3 * 82,
+      serialLength: {
+        episodes: 3,
+        length: 82,
+      },
+    })
+  })
+
+  it('Joan of Arc', function () {
+    const content = fs.readFileSync(`${__dirname}/sites/JoanOfArc.html`)
+    const parsed = filmParser(content)
+
+    chai.assert.containSubset(parsed, {
+      length: 2 * 90,
+      serialLength: {
+        episodes: 2,
+        length: 90,
+      },
+    })
+  })
+})
